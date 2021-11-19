5f5ddad00c69400b0641aa615cd670a04b5e7134
Saif-eddeen <sdsdseao99@hotmail.com>
2021-11-19 20:21:33 +0200

    Homework 1,2,3 of android practical(first three interfaces)

---
+++ app/src/main/java/com/example/myapplication/LoginAct.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_login);
+    }
+}
