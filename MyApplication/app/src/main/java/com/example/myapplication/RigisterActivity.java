155ea601b84ff00beedf9dd54cccbc61ffbb976b
Saif-eddeen <sdsdseao99@hotmail.com>
2021-11-27 00:14:25 +0200

    new interface

---
+++ MyApplication/app/src/main/java/com/example/myapplication/RigisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RigisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_rigister);
+    }
+}
